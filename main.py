7c216168dd5c920242abd8a2aab0a9d46e1e6fa6
Mamitiana RASOLOJAONA <115560388+miragefr0st@users.noreply.github.com>
2023-04-17 21:58:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 import pygame
 import random
 
+
 # Initialize Pygame
 pygame.init()
 
 # Set up the display
-width, height = 1280, 720
+width, height = 1280, 1080
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Endless Scroll")
 
-backround_image = pygame.image.load("background1.png").convert()
-backround_rect = backround_image.get_rect()
-backround_rect.left = width + 50
-backround_rect.bottom = height - 1
+#load background image
+background_size = (width, height)
+background = pygame.image.load("bg1.png","bg2.png").convert()
+background = pygame.transform.scale(background,background_size)
+background_y=0
+
+#Vitesse fond
+scroll_speed= 2.5
+
 
 # Set up the clock
 clock = pygame.time.Clock()
+FPS= 60
 
 # Load the player sprite
-player_image = pygame.image.load("player.png").convert_alpha()
+player_size = (100,100)
+player_image = pygame.image.load("player1.png").convert_alpha()
+player_image = pygame.transform.scale(player_image,player_size)
 
 # Load the enemy sprite
+enemy_size = (80,80)
 enemy_image = pygame.image.load("enemy.png").convert_alpha()
+enemy_image = pygame.transform.scale(enemy_image,enemy_size)
 
 # Set up the player
 player_rect = player_image.get_rect()
 player_rect.centerx = width // 2
-player_rect.bottom = height - 10
-player_pv = 20
+player_rect.bottom = height - 50
 
 # Set up the bullets
 bullet_list = []
-bullet_speed = 10
+bullet_speed = 20
+
 
 # Set up the enemies
 enemy_list = []
@@ -38,7 +50,6 @@ enemy_speed = 5
 enemy_spawn_rate = 60
 enemy_spawn_counter = 0
 
-# Set up the score
 score = 0
 font = pygame.font.SysFont(None, 24)
 
@@ -53,20 +64,16 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 # Fire a bullet
-                bullet_rect = pygame.Rect(0, 0, 4, 10)
+                bullet_rect = pygame.Rect(0, 0, 5, 5)
                 bullet_rect.centerx = player_rect.centerx
                 bullet_rect.bottom = player_rect.top
                 bullet_list.append(bullet_rect)
-
+    
     # Move the player
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and player_rect.left > 0:
         player_rect.move_ip(-5, 0)
     if keys[pygame.K_RIGHT] and player_rect.right < width:
-        player_rect.move_ip(5, 0)
-    if keys[pygame.K_UP] and player_rect.left > 0:
-        player_rect.move_ip(-5, 0)
-    if keys[pygame.K_DOWN] and player_rect.right < width:
         player_rect.move_ip(5, 0)
 
     # Move the bullets
@@ -86,39 +93,35 @@ while running:
         enemy_list.append(enemy_rect)
         enemy_spawn_counter = 0
 
-    # Detect collisions bullet
+    # Detect collisions
     for bullet_rect in bullet_list:
         for enemy_rect in enemy_list:
             if bullet_rect.colliderect(enemy_rect):
                 enemy_list.remove(enemy_rect)
                 bullet_list.remove(bullet_rect)
                 score += 1
-    
-    
-    # Detect collisions player
-    for enemy_rect in enemy_list:
-        if player_rect.colliderect(enemy_rect):
-            enemy_list.remove(enemy_rect)
-            player_pv -= 5
-            
 
     # Draw the screen
     screen.fill((0, 0, 0))
-    screen.blit(backround_image, backround_rect)
+    background_y+=scroll_speed
+    if background_y>background.get_height():
+        background_y=0
+    screen.blit(background, (0,background_y))
+    screen.blit(background,(0,background_y-background.get_height()))
+            
+    
     screen.blit(player_image, player_rect)
     for bullet_rect in bullet_list:
         pygame.draw.rect(screen, (255, 255, 255), bullet_rect)
     for enemy_rect in enemy_list:
         screen.blit(enemy_image, enemy_rect)
     score_text = font.render("Score: " + str(score), True, (255, 255, 255))
-    screen.blit(score_text, (450, 10))
-    pv_text = font.render("Pv: " + str(player_pv), True, (255,255,255))
-    screen.blit(pv_text, (10,10))
+    screen.blit(score_text, (10, 10))
     pygame.display.flip()
+    pygame.display.update()
 
     # Limit the frame rate
-    clock.tick(60)
+    clock.tick(FPS)
 
 # Clean up
 pygame.quit()
-
